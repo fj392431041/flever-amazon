c38122045b5e8fa1fc79dc8b6363a4d7eaee0444
fj <392431041@qq.com>
2018-10-27 21:40:02 +0800

    初次提交

---
+++ src/main/java/cn/com/flever/amazon/app/AmazonApp.java
@@ -0,0 +1,25 @@
+package cn.com.flever.amazon.app;
+
+
+import cn.com.flever.common.app.BaseApp;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class AmazonApp extends BaseApp {
+
+  public static void main(String[] args){
+    SpringApplication.run(AmazonApp.class,args);
+  }
+
+
+}
---
+++ src/main/java/cn/com/flever/amazon/ui/AmazonController.java
@@ -0,0 +1,15 @@
+package cn.com.flever.amazon.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("amazon")
+public class AmazonController {
+
+
+
+
+}
